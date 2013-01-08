a9bc0a89918035cae90d66c9bcd48bb8f205cf80
James Koppen <jk@jameskoppen>
2013-01-07 19:27:51 -0600

    First commit. Seems like testing on localhost won't cut it anymore.

---
+++ TODO.md
@@ -0,0 +1,8 @@
+Manage presence. 
+Host on github. Authorize domain on FB and Firebase.
+Highlight people who are on the page, 
+grey out people who have the page open but have left the page, 
+remove people who have disconnected entirely or who logout explicitly. 
+Add buttons for login and logout. Links with styling changes work for now. 
+Add Twitter support.
+Add indications in title text of what the opacities mean (clerping, clerped).
---
+++ clerp.js
@@ -0,0 +1,61 @@
+var clerp = new Firebase('https://jiko.firebaseio.com/');
+var authClient = new FirebaseAuthClient(clerp);
+var people = clerp.child("people");
+var uid;
+
+people.on('child_added', function(snapshot) {
+  if (snapshot.val().loggedIn === true) {
+    showUser(snapshot.val().data);
+  }
+});
+
+people.on('child_changed', function(snapshot) {
+  if (snapshot.val().loggedIn === true) {
+    showUser(snapshot.val().data);
+  } else {
+    removeUser(snapshot.val().data.id);
+  }
+});
+
+var signin = document.getElementById("signin");
+signin.addEventListener("click", function() {
+  authClient.login('facebook', function(error, token, user) {
+    if (!error) {
+      uid = user.id;
+      var userRef = people.child(user.id);
+      userRef.once('value', function(snapshot) {
+        if (snapshot.val() === null) {
+          userRef.set({data: user, loggedIn: true});
+        } else {
+          userRef.update({data: user, loggedIn: true});
+        }
+      });
+    } else {
+      console.log(error);
+    }
+  });
+});
+
+var signout = document.getElementById("signout");
+signout.addEventListener("click", function() {
+  people.child(uid).update({loggedIn:false});
+});
+
+var showUser = function (user) {
+  var clerps = document.getElementById('clerps');
+  var li = document.createElement("li");
+  li.setAttribute('id',user.id);
+  var a = document.createElement("a")
+  a.setAttribute('href',user.link);
+  var img = document.createElement("img");
+  img.src="https://graph.facebook.com/" + user.id + "/picture/?type=large&return_ssl_resources=1";
+  img.setAttribute("title",user.name);
+  clerps.appendChild(li);
+  li.appendChild(a);
+  a.appendChild(img);
+}
+
+var removeUser = function (id) {
+  var user = document.getElementById(id);
+  user.parentNode.removeChild(user);
+}
